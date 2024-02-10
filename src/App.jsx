54eca37855e23ca686c61440a31b62798453a014
Saurav Bharti <sauravbharti38@gmail.com>
2024-02-10 08:56:35 +0530

    third msg

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,6 @@ function App() {
       <h1>Welcome to the group...</h1>
       <h1>Hii,I am sonu gupta</h1>
 
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit commodi eaque,
-         voluptatibus quod, corporis quaerat placeat cum doloremque possimus, totam non. 
-         Unde maxime amet, omnis pariatur minus maiores. Dolorem vitae totam debitis natus 
-         vel veniam similique alias saepe aut. Vitae.</p>
     </>
   )
 }
