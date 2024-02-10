74b7f09a12ba25c485c62f245ed79ec4d1f4cb3b
Saurav Bharti <sauravbharti38@gmail.com>
2024-02-10 08:52:36 +0530

    second msg

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
-     <h1>Welcome to the group...</h1>
-     <h1>Hii,I am sonu gupta</h1>
+      <h1>Welcome to the group...</h1>
+      <h1>Hii,I am sonu gupta</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit commodi eaque,
+         voluptatibus quod, corporis quaerat placeat cum doloremque possimus, totam non. 
+         Unde maxime amet, omnis pariatur minus maiores. Dolorem vitae totam debitis natus 
+         vel veniam similique alias saepe aut. Vitae.</p>
     </>
   )
 }
