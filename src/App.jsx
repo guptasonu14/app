40074ef35ca89aef8256b4af9f588300de6133b9
guptasonu14 <sguptabudha@gmail.com>
2024-02-15 09:53:40 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
      <h1>Hello I am Tej Punj Pandey</h1>
      <h1>Hello I am Tej Punj Pandey</h1>
       <p>Let Start the work</p>
+      <h1>hello welcome to my world</h1>
 
 
     </>
